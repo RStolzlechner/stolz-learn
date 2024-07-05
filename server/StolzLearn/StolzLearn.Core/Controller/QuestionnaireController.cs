7dabbf6fe9782a0e870cd86d02bd8211b8a25adb
RStolzlechner <ricardo.stolzlechner@yoshie.io>
2024-07-05 19:21:43 +0200

    questionare controller

--- server/StolzLearn/StolzLearn.Core/Controller/QuestionnaireController.cs
+++ server/StolzLearn/StolzLearn.Core/Controller/QuestionnaireController.cs
@@ -2,19 +2,38 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using StolzLearn.Core.Models;
 using StolzLearn.Core.Services;
+using StolzLearn.Core.SignalR;
 
 namespace StolzLearn.Core.Controller;
 
 [Route("api/v1/[controller]")]
 public class QuestionnaireController(
+    ICourseService courseService,
+    IQuestionService questionService,
     IQuestionnaireService questionnaireService,
-    IStatisticService statisticService) : ControllerBase
+    IStatisticService statisticService,
+    IHubContext<CoreHub, ICoreHub> hub) : ControllerBase
 {
     [HttpPost]
     public async Task<IActionResult> AddQuestionnaire([FromBody] Questionnaire questionnaire)
     {
-        //todo
+        var courseId = await courseService.SelectByIds([questionnaire.CourseId]);
+        if(courseId.Count() != 1)
+            return BadRequest("Given course id not found in database");
         
-        return Ok(new {});
+        var givenQuestionIds = questionnaire.Answers.Select(a => a.QuestionId).ToList();
+        var questionIds = await questionService.SelectByIds(givenQuestionIds);
+        if(questionIds.Count() != givenQuestionIds.Count())
+            return BadRequest("Not all question ids found in database");
+        
+        if(questionnaire.Answers.Any(a => string.IsNullOrEmpty(a.GivenAnswer)))
+            return BadRequest("Given answer is empty");
+        
+        await questionnaireService.Insert(questionnaire);
+        var statistic = await statisticService.SelectQuestionnaireStatistics(givenQuestionIds);
+
+        await hub.Clients.All.OnCourseChanged(questionnaire.CourseId);
+        
+        return Ok(statistic);
     }
 }
